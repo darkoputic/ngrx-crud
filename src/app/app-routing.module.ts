0d369e09b5902c68f6f29ce1333b689c4836bb6b
darkoputic <dputic93@gmail.com>
2021-11-23 16:46:58 +0100

    auth

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,6 +14,10 @@ const routes: Routes = [
     component: CounterComponent,
   },
   {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule),
+  },
+  {
     path: '**',
     component: NotFoundPageComponent
   }
